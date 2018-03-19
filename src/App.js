ef6a74cbc2ba82d538b6cbc99093a7640bb03e53
Diego Almiro <diego.almiron@claro.com.ar>
2018-03-19 18:54:07 -0300

    added SASS for bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
 
 import MyAppNavbar from './components/MyAppNavbar';
 import MyAppAside from './components/MyAppAside';
--- src/components/common/NavbarFilter.js
+++ src/components/common/NavbarFilter.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
 import {
   InputGroup,
@@ -8,19 +7,34 @@ import {
   Input
  } from 'reactstrap';
 
-const NavbarFilter = (props) => {
-  const { onChange } = props;
+class NavbarFilter extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <InputGroup className="w-50 p-2">
-      <Input type="email" name="email" id="exampleEmail" placeholder="buscar..." onChange={onChange} />
-      <InputGroupAddon addonType="append"><Button><span className="oi oi-eye"></span></Button></InputGroupAddon>
-    </InputGroup>
-  );
-}
+    this.state = {
+      input: ''
+    }
+  }
 
-NavbarFilter.propTypes = {
-  onChange: PropTypes.func
+  onInputChangeHandler = (event) => {
+    this.setState({input: event.target.value});
+    console.log('filter change: ', event.target.value);
+  }
+
+  render() {
+    return (
+      <InputGroup className="w-50 p-2">
+        <Input
+          type="email"
+          name="email"
+          id="exampleEmail"
+          placeholder="buscar..."
+          onChange={this.onInputChangeHandler}
+          value={this.state.input} />
+        <InputGroupAddon addonType="append"><Button><span className="oi oi-eye"></span></Button></InputGroupAddon>
+      </InputGroup>
+    );
+  }
 }
 
 export default NavbarFilter;
